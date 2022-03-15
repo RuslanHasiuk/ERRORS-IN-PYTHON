64c78e3d737083b82b7b972c072af12b6b0abc2c
RuslanHasiuk <54449805+RuslanHasiuk@users.noreply.github.com>
2022-03-15 15:41:27 +0200

    Update Key_Takeaways.py

--- Key_Takeaways.py
+++ Key_Takeaways.py
@@ -1,3 +1,5 @@
-print('''SyntaxError: Error caused by not following the proper structure (syntax) of the language.
+print('''
+SyntaxError: Error caused by not following the proper structure (syntax) of the language.
 NameError: Errors reported when the interpreter detects an object that is unknown.
-TypeError: Errors thrown when an operation is applied to an object of an inappropriate type.''')
+TypeError: Errors thrown when an operation is applied to an object of an inappropriate type.
+''')
